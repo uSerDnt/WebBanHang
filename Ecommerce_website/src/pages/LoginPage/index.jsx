f4ce11590fbc1955fb2cab70f75f781870a19722
ntnguyen0903 <ntnguyen09032001@gmail.com>
2024-01-26 21:37:33 +0700

    remove login

--- Ecommerce_website/src/pages/LoginPage/index.jsx
+++ Ecommerce_website/src/pages/LoginPage/index.jsx
@@ -1,116 +1,220 @@
-import React, { useState } from "react";
-import { Input, Button, Form } from "antd";
-import { useNavigate } from "react-router-dom";
-import { auth } from "../../../FirebaseConfig";
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-} from "firebase/auth";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import React from "react";
+// Make sure to replace 'COVER_IMAGE' and 'path/to/your/image' with your actual image path or import
+import loginImg from "../../assets/login.jpg";
+import GOOGLE_ICON from "../../assets/google.png"
+const colors = {
+  primary: "#e06666",
+  background: "#f5f5f5",
+  disabled: "#d9d9d9",
+};
+
 const Login = () => {
-  const [form] = Form.useForm(); //tui khong biet
-  const navigate = useNavigate();
-  const [login, setLogin] = useState(true);
-
-  const handleLogin = async (values) => {
-    try {
-      const { username, password, repassword } = values;
-      if (login) {
-        // Sign in
-        await signInWithEmailAndPassword(auth, username, password);
-        navigate("/");
-        toast.success("Đăng nhập thành công");
-      } else {
-        // Sign up
-        if (password === repassword) {
-          await createUserWithEmailAndPassword(auth, username, password);
-          window.location.reload();
-          toast.success("Đăng ky thành công");
-        } else {
-          toast.error("lỗi sever");
-          window.location.reload();
-          // Handle password mismatch error
-        }
-      }
-    } catch (error) {
-      console.error("Authentication Error:", error.message);
-      // Handle authentication errors (e.g., display error message)
-    }
-  };
-
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50">
-      <div className="max-w-md w-full space-y-8 p-8 bg-white rounded-lg shadow-md">
-        <h2 className="text-3xl font-extrabold text-gray-800">
-          {login ? "Login" : "Sign Up"}
-        </h2>
+    <div className="w-full h-screen flex items-start">
+      <div className="relative w-1/2 h-full flex flex-col">
+        <div className="absolute top-[20%] left-[10%] flex flex-col">
+          <h1 className="text-4xl text-white font-bold my-4">
+            Turn Your Ideas into reality
+          </h1>
+          <p className="text-xl text-white font-normal">
+            Start for free and get attractive offers from the community
+          </p>
+        </div>
+        <img src={loginImg} className="w-full h-full object-cover" />
+      </div>
 
-        <Form
-          form={form}
-          name="loginForm"
-          onFinish={handleLogin}
-          className="mt-8 space-y-6"
-        >
-          <Form.Item
-            label="Username"
-            name="username"
-            rules={[{ required: true, message: "Please enter your username" }]}
-          >
-            <Input />
-          </Form.Item>
+      <div className="w-1/2 h-full bg-[#f5f5f5] flex flex-col p-20 justify-between items-center">
+        <h1 className="w-full max-w-[500px] mx-auto text-xl text-[#060606] font-semibold mr-auto">
+          Interactive Brand
+        </h1>
 
-          <Form.Item
-            label="Password"
-            name="password"
-            rules={[{ required: true, message: "Please enter your password" }]}
-          >
-            <Input.Password />
-          </Form.Item>
+        <div className="w-full flex flex-col max-w-[500px]">
+          <div className="w-full flex flex-col mb-2">
+            <h3 className="text-3xl font-semibold mb-2">Đăng Nhập</h3>
+            <p className="text-base mb-2">
+              Welcome Back! Please enter your details.
+            </p>
+          </div>
 
-          {!login && ( // Show Repassword field only when login is false
-            <Form.Item
-              label="Repassword"
-              name="repassword"
-              rules={[
-                { required: true, message: "Please re-enter your password" },
-                ({ getFieldValue }) => ({
-                  validator(_, value) {
-                    if (!value || getFieldValue("password") === value) {
-                      return Promise.resolve();
-                    }
-                    return Promise.reject(
-                      new Error("The two passwords do not match"),
-                    );
-                  },
-                }),
-              ]}
-            >
-              <Input.Password />
-            </Form.Item>
-          )}
+          <div className=" w-full flex-col">
+            <input
+              type="email"
+              placeholder="Email"
+              className=" w-full text-black py-2 my-2 bg-transparent border-b border-black outline-none focus:outline-none"
+            />
 
-          <Form.Item>
-            <Button
-              type="primary"
-              htmlType="submit"
-              className="w-full bg-primary"
-            >
-              {login ? "Log in" : "Sign up"}
-            </Button>
-          </Form.Item>
+            <input
+              type="password"
+              placeholder="Password"
+              className=" w-full text-black py-2 my-2 bg-transparent border-b border-black outline-none focus:outline-none"
+            />
+          </div>
+          <div className="w-full flex items-center justify-between">
+            <div className="w-full flex items-center">
+              <input type="checkbox" className="w-4 h-4 mr-2" />
+              <p className="text-sm">Remember me for 30 days</p>
+            </div>
+            <p className="text-sm font-medium whitespace-nowrap cursor-pointer underline underline-offset-2">
+              Forgot Password ?
+            </p>
+          </div>
 
-          <Form.Item>
-            <Button type="link" onClick={() => setLogin(!login)}>
-              {login
-                ? "Don't have an account? Sign up"
-                : "Already have an account? Log in"}
-            </Button>
-          </Form.Item>
-        </Form>
+          <div className="w-full my-4">
+            <div className="my-2">
+              <button className="w-full text-white font-semibold bg-[#060606] rounded-md p-4 text-center cursor-pointer">
+                Log in
+              </button>
+            </div>
+            <div className="my-2">
+              <button className="w-full text-[#060606] font-semibold bg-white border border-black rounded-md p-4 text-center cursor-pointer">
+                Register
+              </button>
+            </div>
+          </div>
+
+          <div className="w-full flex items-center justify-center relative py-2">
+              <div className="w-full h-[1px] bg-black/40"></div>
+              <p className="text-lg absolute text-black/80 bg-[#f5f5f5] px-2">
+                or
+              </p>
+            </div>
+            <div className="w-full text-[#060606] my-2 font-semibold bg-white border border-black/40 rounded-md p-4 text-center flex items-center justify-center cursor-pointer">      
+             <img src={GOOGLE_ICON} className="h-6 mr-2" />
+             Sign In With Google
+            </div>
+        </div>
+
+        <div className="w-full flex items-center justify-center">
+          <p className="text-sm font-normal text-[#060606]">
+            Dont have account
+            <span className="text- font-semibold underline underline-offset-2 cursor-pointer">
+              Sign up for free
+            </span>
+          </p>
+        </div>
       </div>
     </div>
   );
 };
 
 export default Login;
+
+
+
+
+// import React, { useState } from "react";
+// import { Input, Button, Form } from "antd";
+// import { useNavigate } from "react-router-dom";
+// import { auth } from "../../../FirebaseConfig";
+// import {
+//   createUserWithEmailAndPassword,
+//   signInWithEmailAndPassword,
+// } from "firebase/auth";
+// import { ToastContainer, toast } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+// const Login = () => {
+//   const [form] = Form.useForm(); //tui khong biet
+//   const navigate = useNavigate();
+//   const [login, setLogin] = useState(true);
+
+//   const handleLogin = async (values) => {
+//     try {
+//       const { username, password, repassword } = values;
+//       if (login) {
+//         // Sign in
+//         await signInWithEmailAndPassword(auth, username, password);
+//         navigate("/");
+//         toast.success("Đăng nhập thành công");
+//       } else {
+//         // Sign up
+//         if (password === repassword) {
+//           await createUserWithEmailAndPassword(auth, username, password);
+//           window.location.reload();
+//           toast.success("Đăng ky thành công");
+//         } else {
+//           toast.error("lỗi sever");
+//           window.location.reload();
+//           // Handle password mismatch error
+//         }
+//       }
+//     } catch (error) {
+//       console.error("Authentication Error:", error.message);
+//       // Handle authentication errors (e.g., display error message)
+//     }
+//   };
+
+//   return (
+//     <div className="min-h-screen flex items-center justify-center bg-gray-50">
+//       <div className="max-w-md w-full space-y-8 p-8 bg-white rounded-lg shadow-md">
+//         <h2 className="text-3xl font-extrabold text-gray-800">
+//           {login ? "Login" : "Sign Up"}
+//         </h2>
+
+//         <Form
+//           form={form}
+//           name="loginForm"
+//           onFinish={handleLogin}
+//           className="mt-8 space-y-6"
+//         >
+//           <Form.Item
+//             label="Username"
+//             name="username"
+//             rules={[{ required: true, message: "Please enter your username" }]}
+//           >
+//             <Input />
+//           </Form.Item>
+
+//           <Form.Item
+//             label="Password"
+//             name="password"
+//             rules={[{ required: true, message: "Please enter your password" }]}
+//           >
+//             <Input.Password />
+//           </Form.Item>
+
+//           {!login && ( // Show Repassword field only when login is false
+//             <Form.Item
+//               label="Repassword"
+//               name="repassword"
+//               rules={[
+//                 { required: true, message: "Please re-enter your password" },
+//                 ({ getFieldValue }) => ({
+//                   validator(_, value) {
+//                     if (!value || getFieldValue("password") === value) {
+//                       return Promise.resolve();
+//                     }
+//                     return Promise.reject(
+//                       new Error("The two passwords do not match"),
+//                     );
+//                   },
+//                 }),
+//               ]}
+//             >
+//               <Input.Password />
+//             </Form.Item>
+//           )}
+
+//           <Form.Item>
+//             <Button
+//               type="primary"
+//               htmlType="submit"
+//               className="w-full bg-primary"
+//             >
+//               {login ? "Log in" : "Sign up"}
+//             </Button>
+//           </Form.Item>
+
+//           <Form.Item>
+//             <Button type="link" onClick={() => setLogin(!login)}>
+//               {login
+//                 ? "Don't have an account? Sign up"
+//                 : "Already have an account? Log in"}
+//             </Button>
+//           </Form.Item>
+//         </Form>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Login;
